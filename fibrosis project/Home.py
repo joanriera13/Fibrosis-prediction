051861a02dbd3975cab4c9425ba4a5a333c69273
Joan Riera Tur <joan.riera.tur@estudiantat.upc.edu>
2024-12-15 11:56:35 +0100

    Home.py

--- fibrosis project/Home.py
+++ fibrosis project/Home.py
@@ -24,15 +24,6 @@ def show_home():
             font-size: 16px;              /* Tamaño de la fuente */
             cursor: pointer;              /* Cambiar cursor */
         }
-        .stImage {
-            display: flex;
-            margin-left: auto;
-            margin-right: auto;
-            width: 30%;
-            max-width: 300px;
-            height: auto;
-            justify-content: center !important;
-        }
         </style>
         """, unsafe_allow_html=True)
 
@@ -40,9 +31,7 @@ def show_home():
     st.markdown('<p class="title">Lung Fibrosis Progression Prediction</p>', unsafe_allow_html=True)
 
     st.markdown("<p class='center-text'>LFPP is a web application that predicts the progression of a patient's lung fibrosis illness. This web uses a machine learning model trained on a dataset of patients with lung fibrosis in Spain. The model takes as input various features of the patient that you shall provide to predict the development of the patient.</p>", unsafe_allow_html=True)
-
-    st.markdown("<p class='stImage'>diagnosis.png</p>", unsafe_allow_html=True)
-    
+        
     # Botón funcional con diseño personalizado
     if st.button("Start the questionnaire"):
         st.session_state.current_page = "Questionnaire"
